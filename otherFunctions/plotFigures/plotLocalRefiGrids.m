3236a6aef25105fb0a85ca2c5ae7c68f2be76c48
thinkvantagedu <duxiaohan2006@icloud.com>
2018-02-21 16:13:49 +0000

    minor update: modified plotLocalRefiGrids.m. 	modified:   otherFunctions/plotFigures/plotLocalRefiGrids.m

--- otherFunctions/plotFigures/plotLocalRefiGrids.m
+++ otherFunctions/plotFigures/plotLocalRefiGrids.m
@@ -12,40 +12,41 @@ dx = 0.5;
 dy = 1.2;
 plotScatter = ...
     @(iScat, jScat,  inpt) ...
-    scatter(inpt{iScat}(jScat, 2), inpt{iScat}(jScat, 3), 30, 'b', 'filled');
+    scatter(inpt{iScat}(jScat, 2), inpt{iScat}(jScat, 3), 20, 'b', 'filled');
 textScatter = ...
     @(iScat, jScat,  inpt) ...
     text(inpt{iScat}(jScat, 2) + dx, inpt{iScat}(jScat, 3) + dy, ...
     num2str(inpt{iScat}(jScat, 1)));
-no.plot = 4;
+nplot = 5;
 pmEXP.hhatBlk = GSAGridtoBlockwithIndx(pmEXP.idx.inpt);
 nhatBlk = length(pmEXP.hhatBlk);
 % loop random point, create refined pm grid and plot.
-for iPlot = 1:no.plot
+pmPos = [10 15; 33 3; 35 43; 5 31; 20 18];
+for iPlot = 1:nplot
     %     generate the refined grid.
-    pmEXP.maxLoc1 = randi([0, 50],1, 1);
-    pmEXP.maxLoc2 = randi([0, 50],1, 1);
+    pmEXP.loc1 = pmPos(iPlot, 1);
+    pmEXP.loc2 = pmPos(iPlot, 2);
     
     % plot hat blocks.
     pmEXP.hatBlk = pmEXP.hhatBlk;
-    subplot(2, 4, iPlot + 4)
+    subplot(2, nplot, iPlot + nplot);
     for iScat = 1:length(pmEXP.hatBlk)
         for jScat = 1:4
             plotScatter(iScat, jScat, pmEXP.hatBlk);
         end
         hold on
     end
-    scatter(pmEXP.maxLoc1, pmEXP.maxLoc2, 50, 'r', 'd', 'filled');
-    text(pmEXP.maxLoc1 + 2, pmEXP.maxLoc2, ...
+    scatter(pmEXP.loc1, pmEXP.loc2, 30, 'r', 'd', 'filled');
+    text(pmEXP.loc1 + 2, pmEXP.loc2, ...
         num2str(iPlot), 'Color', 'r', 'FontSize', 20);
     grid minor
+    axis([0 50 0 50])
     axis square
     [pmEXP.hhatBlk, pmEXP.otptRaw] = GSARefineGridLocalwithIdx...
-        (pmEXP.hhatBlk, pmEXP.maxLoc1, pmEXP.maxLoc2);
-    
+        (pmEXP.hhatBlk, pmEXP.loc1, pmEXP.loc2);
     % plot hhat blocks.
     %     plot the grid.
-    subplot(2, 4, iPlot)
+    subplot(2, nplot, iPlot);
     
     for iScat = 1:length(pmEXP.hhatBlk)
         for jScat = 1:4
@@ -53,14 +54,13 @@ for iPlot = 1:no.plot
         end
         hold on
     end    
-    scatter(pmEXP.maxLoc1, pmEXP.maxLoc2, 50, 'r', 'd', 'filled');
-    text(pmEXP.maxLoc1 + 2, pmEXP.maxLoc2, ...
+    scatter(pmEXP.loc1, pmEXP.loc2, 30, 'r', 'd', 'filled');
+    text(pmEXP.loc1 + 2, pmEXP.loc2, ...
         num2str(iPlot), 'Color', 'r', 'FontSize', 20);
     grid minor
+    axis([0 50 0 50])
     axis square
-    
     nhatBlk = length(pmEXP.hhatBlk);
-    
 end
 
 
